0b5df1994bfb4057bd9ef8453dad5116f02a7b11
Adrian Rabelo <adrianrabeloe10@gmail.com>
2021-07-07 13:12:59 -0400

    tratando erros

--- index.js
+++ index.js
@@ -12,147 +12,151 @@ const fs = require("fs");
 client.commands = new Discord.Collection();
 
 const commandFiles = fs
-	.readdirSync("./commands/")
-	.filter((file) => file.endsWith(".js"));
+    .readdirSync("./commands/")
+    .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
+    const command = require(`./commands/${file}`);
 
-	client.commands.set(command.name, command);
+    client.commands.set(command.name, command);
 }
 client.on("ready", () => {
-	console.log("Tô pronto");
+    console.log("Tô pronto");
 
-	// command(client, "teste", (message) => {
-	// 	const content = message.content.replace("!teste", "").trim();
-	// 	if (message.member.hasPermission("ADMINISTRATOR")) {
-	// 		mutedRole = message.guild.roles.cache.find(
-	// 			(role) => role.name === content
-	// 		);
-	// 	}
-	// });
+    // command(client, "teste", (message) => {
+    // 	const content = message.content.replace("!teste", "").trim();
+    // 	if (message.member.hasPermission("ADMINISTRATOR")) {
+    // 		mutedRole = message.guild.roles.cache.find(
+    // 			(role) => role.name === content
+    // 		);
+    // 	}
+    // });
 
-	command(client, "server", (message) => {
-		var server = message.guild;
-		message.channel.send(`Nome do servidor: 
+    command(client, "server", (message) => {
+        var server = message.guild;
+        message.channel.send(`Nome do servidor: 
         ${server.name}
 tem um total de ${server.memberCount} cabeças de pika`);
-	});
+    });
 
-	// command(client, ["cc", "clearchannel"], (message) => {
-	// 	if (message.member.hasPermission("ADMINISTRATOR")) {
-	// 		message.channel.messages.fetch().then((results) => {
-	// 			message.channel.bulkDelete(results);
-	// 		});
-	// 	}
-	// });
+    // command(client, ["cc", "clearchannel"], (message) => {
+    // 	if (message.member.hasPermission("ADMINISTRATOR")) {
+    // 		message.channel.messages.fetch().then((results) => {
+    // 			message.channel.bulkDelete(results);
+    // 		});
+    // 	}
+    // });
 
-	command(client, "status", (message) => {
-		const content = message.content.replace("!status", "");
-		// if (message.member.hasPermission("ADMINISTRATOR")) {
-		client.user.setPresence({
-			activity: {
-				name: content,
-				type: 0,
-			},
-		});
-		// }
-	});
+    command(client, "status", (message) => {
+        const content = message.content.replace("!status", "");
+        // if (message.member.hasPermission("ADMINISTRATOR")) {
+        client.user.setPresence({
+            activity: {
+                name: content,
+                type: 0,
+            },
+        });
+        // }
+    });
 
-	// command(client, "criartexto", (message) => {
-	// 	const name = message.content.replace("!createtextchannel", "");
-	// 	message.guild.channels
-	// 		.create(name, {
-	// 			type: "text",
-	// 		})
-	// 		.then((channel) => {
-	// 			const categoryId = "822955129913212929";
-	// 			channel.setParent(categoryId);
-	// 		});
-	// });
+    // command(client, "criartexto", (message) => {
+    // 	const name = message.content.replace("!createtextchannel", "");
+    // 	message.guild.channels
+    // 		.create(name, {
+    // 			type: "text",
+    // 		})
+    // 		.then((channel) => {
+    // 			const categoryId = "822955129913212929";
+    // 			channel.setParent(categoryId);
+    // 		});
+    // });
 
-	// command(client, "criarvoz", (message) => {
-	// 	const name = message.content.replace("!criarvoz", "");
-	// 	message.guild.channels
-	// 		.create(name, {
-	// 			type: "voice",
-	// 		})
-	// 		.then((channel) => {
-	// 			const categoryId = "822955129913212929";
-	// 			channel.setParent(categoryId);
-	// 			channel.setUserLimit(10);
-	// 		});
-	// });
+    // command(client, "criarvoz", (message) => {
+    // 	const name = message.content.replace("!criarvoz", "");
+    // 	message.guild.channels
+    // 		.create(name, {
+    // 			type: "voice",
+    // 		})
+    // 		.then((channel) => {
+    // 			const categoryId = "822955129913212929";
+    // 			channel.setParent(categoryId);
+    // 			channel.setUserLimit(10);
+    // 		});
+    // });
 
-	command(client, "embed", (message) => {
-		const logo = "https://imgur.com/gallery/dutQtZ8";
+    command(client, "embed", (message) => {
+        const logo = "https://imgur.com/gallery/dutQtZ8";
 
-		const embed = new Discord.MessageEmbed()
-			.setTitle("Exemplo de texto embutido")
-			.setURL("https://www.google.com.br")
-			.setAuthor(message.author.username)
-			.addFields(
-				{
-					name: "Field 1",
-					value: "Alo Mundim",
-					inline: true,
-				},
-				{
-					name: "Field 2",
-					value: "Alo mundim",
-					inline: false,
-				}
-			);
+        const embed = new Discord.MessageEmbed()
+            .setTitle("Exemplo de texto embutido")
+            .setURL("https://www.google.com.br")
+            .setAuthor(message.author.username)
+            .addFields(
+                {
+                    name: "Field 1",
+                    value: "Alo Mundim",
+                    inline: true,
+                },
+                {
+                    name: "Field 2",
+                    value: "Alo mundim",
+                    inline: false,
+                },
+            );
 
-		message.channel.send(embed);
-	});
+        message.channel.send(embed);
+    });
 
-	command(client, "ping", (message) => {
-		const args = message.content.slice(config.prefix.length).split(/ +/);
-		client.commands.get("ping").execute(message, client);
-		// 	message.channel.send(
-		// 		`Teu ping é ${
-		// 			message.createdTimestamp - Date.now()
-		// 		}ms\nLatência da API ${Math.round(client.ws.ping)}ms`
-		// 	);
-	});
+    command(client, "ping", (message) => {
+        const args = message.content.slice(config.prefix.length).split(/ +/);
+        client.commands.get("ping").execute(message, client);
+        // 	message.channel.send(
+        // 		`Teu ping é ${
+        // 			message.createdTimestamp - Date.now()
+        // 		}ms\nLatência da API ${Math.round(client.ws.ping)}ms`
+        // 	);
+    });
 
-	command(client, "serverinfo", (message) => {
-		message.channel.send("Ta vino");
-	});
+    command(client, "serverinfo", (message) => {
+        message.channel.send("Ta vino");
+    });
 
-	command(client, "tts", async (message) => {
-		const args = message.content.replace("!tts", "");
-		client.commands.get("tts").execute(message, args, client);
-	});
+    command(client, "tts", async (message) => {
+        const args = message.content.replace("!tts", "");
+        client.commands.get("tts").execute(message, args, client);
+    });
 
-	// privateMessage(client, "ping", "Pong!");
+    // privateMessage(client, "ping", "Pong!");
 });
 
 client.on("message", (message) => {
-	if (!message.content.startsWith(config.prefix) || message.author.bot)
-		return;
+    try {
+        if (!message.content.startsWith(config.prefix) || message.author.bot)
+            return;
 
-	const args = message.content.slice(config.prefix.length).split(/ +/);
-	const command = args.shift().toLowerCase();
+        const args = message.content.slice(config.prefix.length).split(/ +/);
+        const command = args.shift().toLowerCase();
 
-	if (command === "clear") {
-		client.commands.get("clear").execute(message, args);
-	} else if (command === "play") {
-		client.commands.get("play").execute(message, args);
-	} else if (command === "leave") {
-		client.commands.get("leave").execute(message, args);
-	} else if (command === "mute") {
-		client.commands.get("mute").execute(message, args);
-	} else if (command === "unmute") {
-		client.commands.get("unmute").execute(message, args);
-	} else if (command === "sucumbir") {
-		client.commands.get("sucumbir").execute(message, args);
-	} else if (command === "perdoar") {
-		client.commands.get("perdoar").execute(message, args);
-	} else if (command == "help") {
-		client.commands.get("help").execute(message, args);
-	}
+        if (command === "clear") {
+            client.commands.get("clear").execute(message, args);
+        } else if (command === "play") {
+            client.commands.get("play").execute(message, args);
+        } else if (command === "leave") {
+            client.commands.get("leave").execute(message, args);
+        } else if (command === "mute") {
+            client.commands.get("mute").execute(message, args);
+        } else if (command === "unmute") {
+            client.commands.get("unmute").execute(message, args);
+        } else if (command === "sucumbir") {
+            client.commands.get("sucumbir").execute(message, args);
+        } else if (command === "perdoar") {
+            client.commands.get("perdoar").execute(message, args);
+        } else if (command == "help") {
+            client.commands.get("help").execute(message, args);
+        }
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 client.login(config.token);
