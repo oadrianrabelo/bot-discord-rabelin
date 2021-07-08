d56a195a85a1f0bbe25b74d773874f7d43838fc7
Adrian Rabelo <adrianrabeloe10@gmail.com>
2021-07-07 23:38:11 -0400

    lapidando comando status

--- commands/status.js
+++ commands/status.js
@@ -1,36 +1,75 @@
+const Discord = require("discord.js");
 module.exports = {
     name: "status",
     description: "Altera o status do bot",
     execute(message, args, client) {
-        const changeStatus = {
-            activity: args[0],
-            type: args[1].toString().replace(",", " "),
-        };
-        client.user.setActivity(
-            changeStatus.type.toString().replace(",", " "),
-            {
-                type: changeStatus.activity,
-            },
-        );
-        // client.user.setPresence({
-        //     activity: {
-        //         name: args.toString().replace(",", " "),
-        //         type: "STREAMING",
-        //     },
-        // });
+        const logo =
+            "https://cdn.discordapp.com/avatars/822620300608274482/20c3c0644c9100ee7837c4d79b8251ec.png?size=256";
+        const embed = new Discord.MessageEmbed()
+            .addField("Jogando", "!status jogando minecraft")
+            .addField("Assistindo", "!status assistindo minecraft")
+            .addField("Transmitindo", "!status transmitindo minecraft")
+            .addField("Ouvindo", "!status ouvindo minecraft")
+            .addField("Competindo", "!status competindo minecraft")
+            .setTitle("Atividades disponíveis")
+            .setTimestamp()
+            .setFooter("BotRabelin", logo)
+            .setColor("#3F72K");
+        try {
+            if (!args[1]) {
+                return (
+                    message.reply(":thinking: tendi não"),
+                    message.channel.send(embed)
+                );
+            }
+            const changeStatus = {
+                type: args[0],
+                activity: args,
+            };
+            const activityTypes = {
+                jogando: "PLAYING",
+                stremando: "STREAMING",
+                ouvindo: "LISTENING",
+                assistindo: "WATCHING",
+                competindo: "COMPETING",
+            };
 
-        message.channel.send("Status do bot alterado com sucesso");
+            let statusConstant = "";
+            //#region
+            switch (changeStatus.type.toString().toLowerCase()) {
+                case "jogando":
+                    statusConstant = activityTypes.playing;
+                    break;
+                case "assistindo":
+                    statusConstant = activityTypes.assistindo;
+                    break;
+                case "ouvindo":
+                    statusConstant = activityTypes.ouvindo;
+                    break;
+                case "transmitindo":
+                    statusConstant = activityTypes.stremando;
+                    break;
+                case "competindo":
+                    statusConstant = activityTypes.competindo;
+                    break;
+                default:
+                    return message.reply("Atividade inválida");
+                    break;
+            }
+            //#endregion
+            let spliceActivity = changeStatus.activity
+                .slice(1)
+                .toString()
+                .split(",")
+                .join(" ");
+            client.user.setActivity(spliceActivity, {
+                type: statusConstant,
+            });
+            message.channel.send("Status do bot alterado com sucesso");
+
+            function setStatusConstant() {}
+        } catch (error) {
+            console.error(error);
+        }
     },
 };
-
-// command(client, "status", (message) => {
-//     const content = message.content.replace("!status", "");
-//     // if (message.member.hasPermission("ADMINISTRATOR")) {
-//     client.user.setPresence({
-//         activity: {
-//             name: content,
-//             type: 0,
-//         },
-//     });
-//     // }
-// });
