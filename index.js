180236c319f1d93ae775967922fe85e287d0aae6
Replit user <replituser@example.com>
2022-04-09 02:38:39 +0000

    reinstalling express

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`App is listening at http://localhost:${port}`));
+
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
