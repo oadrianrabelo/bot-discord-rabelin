4cf4cf271a6d404a23d8243a6c0694d98e56d276
Replit user <replituser@example.com>
2022-04-09 02:41:06 +0000

    changing the message on the view

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Bot iniciado!'));
 
 app.listen(port, () => console.log(`App is listening at http://localhost:${port}`));
 
